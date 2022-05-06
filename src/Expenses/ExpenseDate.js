4026ce844c88e65ddffe10c25a5e73ec5ccc6857
0lgatron <perronolga@gmail.com>
2022-05-06 16:45:43 -0400

    fill Expenses related items

---
+++ src/Expenses/ExpenseDate.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ExpenseDate.css";
+import Card from "../UI/Card";
+
+const ExpenseDate = (props) => {
+  const { date } = props;
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+  return (
+    <Card className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </Card>
+  );
+};
+
+export default ExpenseDate;
---
+++ src/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
+import Card from "../UI/Card";
+import ExpensesFilter from "../FilterExpenses/ExpensesFilter";
+import ExpensesChart from "../Chart/ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const { items } = props;
+  const onSelectedOption = (option) => {
+    setFilteredYear(option);
+  };
+
+  const fileteredExpenses = items.filter((item) => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onSelectedOption={onSelectedOption}
+      />
+      <ExpensesChart expenses={fileteredExpenses} />
+      <ExpensesList items={fileteredExpenses} />
+    </Card>
+  );
+};
+
+export default Expenses;
